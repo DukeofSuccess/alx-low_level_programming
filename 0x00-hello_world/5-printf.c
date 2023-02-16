de64bb18e1e90b79626d603563e41965a6664205
DukeofSuccess <nyeverasimba@gmail.com>
2023-02-16 01:02:50 -0800

    Printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio>
+#include <stdio.h>
 /**
  * main - Prints given phrase
  * Return: 0 if true, non zero otherwise
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n";
+	printf("with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
 }
