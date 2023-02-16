d2143a7882934fb6caa7d47b0e597b11855d2f3d
DukeofSuccess <nyeverasimba@gmail.com>
2023-02-16 02:21:04 -0800

    Qoute

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints given phrase to stderr
+ * Return: 1 for false, 0 for true
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
