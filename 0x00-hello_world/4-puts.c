2c0d5e81c58c33044299c0a23d82a93e446d5085
DukeofSuccess <nyeverasimba@gmail.com>
2023-02-16 00:45:19 -0800

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main()
-	char str[] = "Programming is like building a multilingual puzzle";
-	puts(str);
-	return 0;
+/**
+ * main - prints the line "Programming is like building a multilingual puzzle"
+ * Return: 0 if executed succesfuly, non zero otherwise
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
