05011af14a6109b935d84163e9adaeeddf383fcb
DukeofSuccess <nyeverasimba@gmail.com>
2023-02-18 08:43:23 -0800

    Numbers base10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - print single dig base 10 nums from 0
+ *Return: 0 always
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
