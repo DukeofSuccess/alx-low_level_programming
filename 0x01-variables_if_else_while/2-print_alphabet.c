ab5ab294347719e34cfdf91383f7b82ac9f324f6
DukeofSuccess <nyeverasimba@gmail.com>
2023-02-18 05:53:57 -0800

    Print Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Print alphabet
+ *Return: 0 always
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
